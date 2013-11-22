1129c478bbb49a76f489765a2695f6436df1a291
Austin Feight <afeight@umich.edu>
2013-11-22 17:27:34 -0500

    clean up

--- 99_problems.hs
+++ 99_problems.hs
@@ -1,38 +1,38 @@
 -- Problem 1
 -- Find the last element of a list.
 myLast :: [a] -> a
-myLast [] = error "Can't call last on an empty list"
-myLast [x] = x
+myLast [] 		= error "Can't call last on an empty list"
+myLast [x] 		= x
 myLast (_:xs) = myLast xs
 
 -- Problem 2
 -- Find the last but one element of a list.
 myLastButOne :: [a] -> a
-myLastButOne [] = error "Can't call last on a list with less than 2 elements"
-myLastButOne [x] = error "Can't call last on a list with less than 2 elements"
-myLastButOne [x,_] = x
+myLastButOne [] 		= error "Can't call last on a list with less than 2 elements"
+myLastButOne [x] 		= error "Can't call last on a list with less than 2 elements"
+myLastButOne [x,_] 	= x
 myLastButOne (_:xs) = myLastButOne xs
 
 -- Problem 3
 -- Find the K'th element of a list. The first element in the list is number 1.
 elementAt :: [a] -> Int -> a
 elementAt xs k
-	| len < k = error "Index out of range"
+	| len < k 	= error "Index out of range"
 	| otherwise = xs !! (k - 1)
-	where len = length xs
+	where len 	= length xs
 
 -- Problem 4
 -- Find the number of elements of a list.
 myLength :: [a] -> Int
-myLength [] = 0
+myLength [] 		= 0
 myLength (_:xs) = 1 + myLength xs
 
 -- Problem 5
 -- Reverse a list.
 myReverse :: [a] -> [a]
-myReverse [] = []
-myReverse [x] = [x]
-myReverse (x:xs) = (last xs) : (myReverse(init xs) ++ x:[])
+myReverse [] 			= []
+myReverse [x] 		= [x]
+myReverse (x:xs) 	= (last xs) : (myReverse(init xs) ++ x:[])
 
 -- Problem 6
 -- Find out whether a list is a palindrome.
@@ -53,11 +53,11 @@ data NestedList a = Elem a | List [NestedList a]
 -- Problem 8
 -- Eliminate consecutive duplicates of list elements
 compress :: (Eq a) => [a] -> [a]
-compress [] = []
-compress [a] = [a]
+compress [] 			= []
+compress [a] 			= [a]
 compress (x:xs)
 	| x == head xs	= compress xs
-	| otherwise		= x:compress xs
+	| otherwise			= x:compress xs
 
 -- Problem 9
 -- Pack consecutive duplicates of list elements into sublists. 
@@ -65,16 +65,17 @@ compress (x:xs)
 pack :: (Eq a) => [a] -> [[a]]
 
 pack [] 	= []
-pack [a]	= [[a]]
-pack xs		= packHelper [[]] xs
+pack [a] 	= [a]
+pack xs		= reps : pack rest 
+	where (reps, rest) = span (== head xs) xs
 
 packHelper :: (Eq a) => [[a]] -> [a] -> [[a]]
 
-packHelper x []				= x
+packHelper x []						= x
 packHelper [[]] (x:xs) 		= packHelper [[x]] xs
 packHelper x (y:xs)
-	| last (last x) == y	= packHelper (init x ++ [y:last x]) xs
-	| otherwise 			= packHelper (x ++ [[y]]) xs
+	| last (last x) == y		= packHelper (init x ++ [y:last x]) xs
+	| otherwise 						= packHelper (x ++ [[y]]) xs
 
 -- Problem 10
 -- Run-length encoding of a list. Use the result of problem P09 to implement 
@@ -86,6 +87,7 @@ encode xs = encodeHelper (pack xs)
 
 encodeHelper :: [[a]] -> [(Int, a)]
 
-encodeHelper [] 		= []
-encodeHelper [x]		= [(length x, head x)]
+encodeHelper [] 			= []
+encodeHelper [x]			= [(length x, head x)]
 encodeHelper (x:xs)		= (length x, head x):encodeHelper xs
+
