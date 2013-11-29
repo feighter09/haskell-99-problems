c323c68470dc4b573c9c26c4b80d5c2a20d47edf
Austin Feight <afeight@umich.edu>
2013-11-29 00:48:43 -0500

    I see merge conflicts O.O

--- 99_problems.hs
+++ 99_problems.hs
@@ -1,12 +1,12 @@
 -- Problem 1
--- Find the last element of a list.
+--(*) Find the last element of a list.
 myLast :: [a] -> a
 myLast [] 		= error "Can't call last on an empty list"
 myLast [x] 		= x
 myLast (_:xs) = myLast xs
 
 -- Problem 2
--- Find the last but one element of a list.
+--(*) Find the last but one element of a list.
 myLastButOne :: [a] -> a
 myLastButOne [] 		= error "Can't call last on a list with less than 2 elements"
 myLastButOne [x] 		= error "Can't call last on a list with less than 2 elements"
@@ -14,7 +14,7 @@ myLastButOne [x,_] 	= x
 myLastButOne (_:xs) = myLastButOne xs
 
 -- Problem 3
--- Find the K'th element of a list. The first element in the list is number 1.
+--(*) Find the K'th element of a list. The first element in the list is number 1.
 elementAt :: [a] -> Int -> a
 elementAt xs k
 	| len < k 	= error "Index out of range"
@@ -22,20 +22,20 @@ elementAt xs k
 	where len 	= length xs
 
 -- Problem 4
--- Find the number of elements of a list.
+--(*) Find the number of elements of a list.
 myLength :: [a] -> Int
 myLength [] 		= 0
 myLength (_:xs) = 1 + myLength xs
 
 -- Problem 5
--- Reverse a list.
+--(*) Reverse a list.
 myReverse :: [a] -> [a]
 myReverse [] 			= []
 myReverse [x] 		= [x]
 myReverse (x:xs) 	= (last xs) : (myReverse(init xs) ++ x:[])
 
 -- Problem 6
--- Find out whether a list is a palindrome.
+--(*) Find out whether a list is a palindrome.
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome xs
 	| length xs <= 1 	= True
@@ -43,12 +43,12 @@ isPalindrome xs
 	where belly ls = tail (init ls)
 
 -- Problem 7
--- Flatten a nested list structure
+--(**) Flatten a nested list structure
 data NestedList a = Elem a | List [NestedList a]
 -- **************************************************
 
 -- Problem 8
--- Eliminate consecutive duplicates of list elements
+--(**) Eliminate consecutive duplicates of list elements
 compress :: (Eq a) => [a] -> [a]
 compress [] 			= []
 compress [a] 			= [a]
@@ -57,7 +57,7 @@ compress (x:xs)
 	| otherwise			= x:compress xs
 
 -- Problem 9
--- Pack consecutive duplicates of list elements into sublists. 
+--(**) Pack consecutive duplicates of list elements into sublists. 
 -- If a list contains repeated elements they should be placed in separate sublists
 pack :: (Eq a) => [a] -> [[a]]
 
@@ -75,7 +75,7 @@ packHelper x (y:xs)
 	| otherwise 						= packHelper (x ++ [[y]]) xs
 
 -- Problem 10
--- Run-length encoding of a list. Use the result of problem P09 to implement 
+--(*) Run-length encoding of a list. Use the result of problem P09 to implement 
 -- the so-called run-length encoding data compression method. Consecutive 
 -- duplicates of elements are encoded as lists (N E) where N is the number of
 -- duplicates of the element E.
@@ -91,7 +91,7 @@ encodeHelper (x:xs)		= (length x, head x):encodeHelper xs
 
 
 -- Problem 14
--- Duplicate the elements of a list.
+--(*) Duplicate the elements of a list.
 dupli :: [a] -> [a]
 dupli [] = []
 dupli [x] = [x,x]
@@ -99,7 +99,7 @@ dupli (x:xs) = [x,x] ++ dupli xs
 
 
 -- Problem 15
--- Replicate the elements of a list a given number of times.
+--(**) Replicate the elements of a list a given number of times.
 -- My solution
 repli :: [a] -> Int -> [a]
 repli [] _ = []
@@ -108,7 +108,17 @@ repli _ n
 	| n < 0 = error "Can't replicate negative times"
 repli (x:xs) n = dups ++ repli xs n 
 	where dups = take n (repeat x)
-
 -- Wow so much better sol'n:
-repli xs n = concatMap (replicate n) xs			-- applies the first arg to each elt of second arg, concatenates results
+repli xs n = concatMap (replicate n) xs			-- applies the first arg to each elt of second arg, concatenates results (i guess)
 
+
+-- Problem 17
+--(*) Split a list into two parts; the length of the first part is given.
+split :: [a] -> Int -> ([a], [a])
+split [] _ = ([], [])
+split (x:xs) n 
+	| n < 0 = error "Can't split on a negative index"
+	| n == 0 = ([], x:xs)
+	| n > length xs = (x:xs, [])
+	| otherwise 	 = (x:(fst res), snd res)
+	where res = split xs (n - 1)
