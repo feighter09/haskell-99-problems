ee693cb76d1fec6d4bfcdf75df4ecf3b59c2686b
Austin Feight <afeight@umich.edu>
2014-04-25 18:47:12 -0400

    21,22

--- 99_problems.hs
+++ 99_problems.hs
@@ -5,6 +5,7 @@ myLast [] 		= error "Can't call last on an empty list"
 myLast [x] 		= x
 myLast (_:xs) = myLast xs
 
+
 -- Problem 2
 --(*) Find the last but one element of a list.
 myLastButOne :: [a] -> a
@@ -13,13 +14,16 @@ myLastButOne [x] 		= error "Can't call last on a list with less than 2 elements"
 myLastButOne [x,_] 	= x
 myLastButOne (_:xs) = myLastButOne xs
 
+
 -- Problem 3
 --(*) Find the K'th element of a list. The first element in the list is number 1.
 elementAt :: [a] -> Int -> a
 elementAt xs k
+	| k < 0			= error "Index out of range"
 	| len < k 	= error "Index out of range"
 	| otherwise = xs !! (k - 1)
 	where len 	= length xs
+
 
 -- Problem 4
 --(*) Find the number of elements of a list.
@@ -27,12 +31,14 @@ myLength :: [a] -> Int
 myLength [] 		= 0
 myLength (_:xs) = 1 + myLength xs
 
+
 -- Problem 5
 --(*) Reverse a list.
 myReverse :: [a] -> [a]
 myReverse [] 			= []
 myReverse [x] 		= [x]
 myReverse (x:xs) 	= (last xs) : (myReverse(init xs) ++ x:[])
+
 
 -- Problem 6
 --(*) Find out whether a list is a palindrome.
@@ -42,10 +48,12 @@ isPalindrome xs
 	| otherwise				= (head xs) == (last xs) && isPalindrome (belly xs)
 	where belly ls = tail (init ls)
 
+
 -- ************************************************** Problem 7 **************************************************
 --(**) Flatten a nested list structure
 data NestedList a = Elem a | List [NestedList a]
 -- **************************************************
+
 
 -- Problem 8
 --(**) Eliminate consecutive duplicates of list elements
@@ -55,6 +63,7 @@ compress [a] 			= [a]
 compress (x:xs)
 	| x == head xs	= compress xs
 	| otherwise			= x:compress xs
+
 
 -- Problem 9
 --(**) Pack consecutive duplicates of list elements into sublists. 
@@ -73,6 +82,7 @@ packHelper [[]] (x:xs) 		= packHelper [[x]] xs
 packHelper x (y:xs)
 	| last (last x) == y		= packHelper (init x ++ [y:last x]) xs
 	| otherwise 						= packHelper (x ++ [[y]]) xs
+
 
 -- Problem 10
 --(*) Run-length encoding of a list. Use the result of problem P09 to implement 
@@ -123,13 +133,7 @@ dropEvery xs x
 -- Problem 17
 --(*) Split a list into two parts; the length of the first part is given.
 split :: [a] -> Int -> ([a], [a])
-split [] _ = ([], [])
-split (x:xs) n 
-	| n < 0					= error "Can't split on a negative index"
-	| n == 0 				= ([], x:xs)
-	| n > length xs = (x:xs, [])
-	| otherwise 	 	= (x:(fst res), snd res)
-	where res = split xs (n - 1)
+split xs n = (take n xs, drop n xs)
 
 
 -- Problem 18
@@ -145,7 +149,6 @@ slice xs x y
 --(**) Rotate a list N places to the left.
 rotate :: [a] -> Int -> [a]
 rotate [] _ = []
---rotate xs 0 = xs
 rotate xs n
 	| n > 0			= rotate (middle ++ last:[first]) (n - 1)
 	| n < 0			= rotate (last:first:middle) (n + 1)
@@ -159,11 +162,42 @@ rotate xs n
 -- Problem 20
 --(*) Remove the K'th element from a list.
 removeAt :: [a] -> Int -> [a]
+removeAt [] _ = []
 removeAt xs n
 	| n < 0					= error "Trying to remove an element out of range"
 	| n > length xs	= error "Trying to remove an element out of range"
 	| otherwise 		= (init front) ++ back
 	where (front, back) = split xs n
+
+
+-- Problem 21
+-- Insert an element at a given position into a list.
+insertAt :: a -> [a] -> Int -> [a]
+insertAt elt list spot
+	| spot < 1						= error "Trying to insert an element out of range"
+	|	spot >= length list	= error "Trying to insert an element out of range"
+	| otherwise						= front ++ [elt] ++ back
+	where (front, back) = split list (spot - 1)
+
+
+-- Problem 22
+-- Create a list containing all integers within a given range.
+range :: Int -> Int -> [Int]
+range first last
+	| first < last	= first:(range (first + 1) last)
+	| first > last	= first:(range (first - 1) last)
+	| otherwise 		= [first]
+
+
+-- Problem 23
+-- Extract a given number of randomly selected elements from a list.
+rnd_select :: [a] -> Int -> IO [a]
+rnd_select [] _ = return []
+
+
+
+
+
 
 
 
